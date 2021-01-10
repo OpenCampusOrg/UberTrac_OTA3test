fd06cf4ca41dab182805f8c14db102cd452be4b3
Huguenin-Elie Steve <steve.huguenin-elie@protonmail.ch>
2021-01-10 23:16:16 +0100

    useless C compatibility check

--- UberTrac_OTA3test.ino
+++ UberTrac_OTA3test.ino
@@ -121,13 +121,6 @@ const char* period[4] = { "1.0", "1.0", "0.5", "2.0" };
 byte l = 0; //*/
 
 // Internal Temperature sensor setup
-#ifdef __cplusplus
-extern "C" {
-#endif
-uint8_t temprature_sens_read();
-#ifdef __cplusplus
-}
-#endif
 uint8_t temprature_sens_read();
 
 
